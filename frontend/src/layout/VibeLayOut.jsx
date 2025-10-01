0a7b8d519c980e36a07a76e6e0f24a69a0966abd
PexArt-web <pexart74@gmail.com>
2025-10-01 04:12:31 +0100

    VibeNest

--- frontend/src/layout/VibeLayOut.jsx
+++ frontend/src/layout/VibeLayOut.jsx
@@ -3,12 +3,21 @@ import Footer from "../pages/Components/Footer";
 import { Outlet } from "react-router-dom";
 import { useEffect } from "react";
 import { clientSocket, socket } from "@/Services/weBSocket";
+import { useAuthContext } from "@/Hooks/useAuthContext";
 
 const VibeLayOut = () => {
-  useEffect(()=>{
-    clientSocket()
-    socket.on()
-  }, [])
+  const { user } = useAuthContext();
+  useEffect(() => {
+    clientSocket();
+    socket.on("connect", () => {
+      const userInfo = { id: user?.user._id };
+      socket.emit("userInfo", userInfo);
+    });
+
+    return () =>{
+      socket.off("connect")
+    }
+  }, [user]);
   return (
     <div>
       <Header />
